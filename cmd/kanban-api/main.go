027fe94ea075144836d4838d02311d25cd79c458
Teo Oliver <teooliver@hollandandbarrett.com>
2024-07-05 10:10:57 +0100

    chore: cleanup

--- cmd/kanban-api/main.go
+++ cmd/kanban-api/main.go
@@ -24,8 +24,6 @@ func main() {
 	// Server run context
 	serverCtx, serverStopCtx := context.WithCancel(context.Background())
 
-	// Listen for syscall signals for process to interrupt/quit
-
 	config, err := bootstrap.Config(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file %w", err)
@@ -41,6 +39,7 @@ func main() {
 	// The HTTP Server
 	server := &http.Server{Addr: "0.0.0.0:3000", Handler: router(deps)}
 
+	// Listen for syscall signals for process to interrupt/quit
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
@@ -64,6 +63,7 @@ func main() {
 		serverStopCtx()
 	}()
 
+	// Run the server
 	err = server.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
 		log.Fatal(err)
