85b946b78d9863eb3c841227e04784fdc0cb483e
Teofilo Lizarralde Oliver <teooliver@teofilos-mbp.home>
2024-07-19 12:25:18 +0100

    chore: cleanup

--- cmd/kanban-api/main.go
+++ cmd/kanban-api/main.go
@@ -64,6 +64,7 @@ func main() {
 
 	// Run the server
 	err = server.ListenAndServe()
+
 	if err != nil && err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
@@ -75,46 +76,3 @@ func main() {
 	<-serverCtx.Done()
 
 }
-
-// func router(deps *bootstrap.AllDeps) http.Handler {
-// 	r := chi.NewRouter()
-
-// 	// A good base middleware stack
-// 	r.Use(middleware.RequestID)
-// 	r.Use(middleware.RealIP)
-// 	r.Use(middleware.Logger)
-// 	r.Use(middleware.Recoverer)
-
-// 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-// 		w.Write([]byte("welcome"))
-// 	})
-
-// 	r.Route("/tasks", func(r chi.Router) {
-// 		// TODO: Add Pagination
-
-// 		r.With(
-// 			httpin.NewInput(postgresutils.PageRequest{}),
-// 		).Get("/", deps.Handlers.TaskHandler.ListTasks)
-// 		r.Post("/", deps.Handlers.TaskHandler.CreateTask)
-// 		r.Delete("/{id}", deps.Handlers.TaskHandler.DeleteTask)
-// 		r.Put("/{id}", deps.Handlers.TaskHandler.UpdateTask)
-// 	})
-
-// 	r.Route("/status", func(r chi.Router) {
-// 		// TODO: Add Pagination
-// 		r.Get("/", deps.Handlers.StatusHandler.ListAllStatus)
-// 		r.Post("/", deps.Handlers.StatusHandler.CreateStatus)
-// 		r.Delete("/{id}", deps.Handlers.StatusHandler.DeleteStatus)
-// 		r.Put("/{id}", deps.Handlers.StatusHandler.UpdateStatus)
-// 	})
-
-// 	r.Route("/users", func(r chi.Router) {
-// 		// TODO: Add Pagination
-// 		r.Get("/", deps.Handlers.UserHandler.ListUsers)
-// 		r.Post("/", deps.Handlers.UserHandler.CreateUser)
-// 		r.Delete("/{id}", deps.Handlers.UserHandler.DeleteUser)
-// 		r.Put("/{id}", deps.Handlers.UserHandler.UpdateUser)
-// 	})
-
-// 	return r
-// }
