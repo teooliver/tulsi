cd230ef8194f15947979afa1dee7de2850daf286
Teofilo Lizarralde Oliver <teooliver@teofilos-mbp.home>
2024-10-18 12:14:28 +0100

    chore: allow cors

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -6,12 +6,23 @@ import (
 	"github.com/ggicci/httpin"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/teooliver/kanban/internal/bootstrap"
 	"github.com/teooliver/kanban/pkg/postgresutils"
 )
 
 func Router(deps *bootstrap.AllDeps) http.Handler {
 	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	// A good base middleware stack
 	r.Use(middleware.RequestID)
