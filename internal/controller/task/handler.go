52c52c791919b47a3b30a7921b4db25d23dc4cd8
Teo Oliver <teo.liz.oli@gmail.com>
2025-01-19 19:40:34 +0000

    chore: wip

--- internal/controller/task/handler.go
+++ internal/controller/task/handler.go
@@ -61,6 +61,7 @@ func (h Handler) ListTasks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(jsonTasks))
 }
